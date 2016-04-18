457b48f8c4775970b00afd60adb12f37a209a253
Daniel Hernandez <dh.danny@hotmail.com>
2016-04-18 13:57:40 -0430

    Remove unneccesaary files from gh-pages

--- utilities_and_modules/__init__.py
+++
@@ -1,2 +0,0 @@
-# empty file required for making the folder a Python package
-# it is here only to be able to access pybank.py
--- utilities_and_modules/reset.py
+++
@@ -1,8 +0,0 @@
-import os
-
-try:
-    os.system("rm stocks.db")
-    os.system("echo > stocks.db")
-    print "Database has been wiped and reset"
-except:
-    print "An unexpected error occured"
