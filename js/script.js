9ce275b3e9ef7671a9a7678b58cff06b154e5fca
Daniel Hernandez <dh.danny@hotmail.com>
2016-06-10 13:09:25 -0400

    Automate copyright notice

--- js/script.js
+++ js/script.js
@@ -1,4 +1,10 @@
 $(document).ready( function () {
   $(".button-collapse").sideNav();
 
+  // for updating the copyright notice
+  var date = new Date();
+  currentYear = date.getFullYear();
+  msg = "Copyright Daniel Hernandez H. " + currentYear + ", under the <a class='white-text' href='https://opensource.org/licenses/MIT'>MIT License</a>";
+
+  $("#copyright-notice").html(msg);
 });
