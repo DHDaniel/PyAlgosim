a0c52f49b746476792ea00d7215fcf00110cae49
Daniel Hernandez <dh.danny@hotmail.com>
2017-06-04 18:39:59 -0400

    Completed refactoring the code. May have some performance issues when compared to last version. Don't know why.

--- PySimulator.py
+++ PySimulator.py
@@ -4,6 +4,23 @@
 import sqlite3
 import os
 import copy
+import json
+
+# helper functions
+def normalize_date(datetime_date):
+    """
+    Returns an integer, of the form YYYY/MM/DD, which is useful when determining the records to pull from the database.
+    """
+    year = datetime_date.year
+    month = datetime_date.month
+    day = datetime_date.day
+
+    daystring = "0" + str(day) if len(str(day)) == 1 else str(day)
+    monthstring = "0" + str(month) if len(str(month)) == 1 else str(month)
+    yearstring = str(year)
+
+    return int(yearstring + monthstring + daystring)
+
 
 class PySimulator:
 
@@ -16,17 +33,18 @@ class PySimulator:
         self.connection = None
         self.cursor = None
         self.variables = optional_variables
+        self.latest_prices = {}
 
         # loading up the ticker list created from generating the database. The path must be correct.
         try:
-            self.ticker_list = open("utilities_and_modules/tickers.json").read()
-            self.ticker_list = json.loads(ticker_list)
+            self.ticker_list = open("./utils/tickers.json").read()
+            self.ticker_list = json.loads(self.ticker_list)
         except:
             raise IOError("The tickers.json file could not be loaded. Please make sure that you have generated the database with the initialize script, and that you are running PySimulator from its original location.")
 
         # verify that database is not empty (wrong path may have created a new database)
         if os.path.isfile(self.database) != True:
-            raise Exception("The path entered is not a database file. Please make sure to run initialize.py to create the stock database, and point to the correct location.")
+            raise Exception("The path entered is not the correct database file. Please make sure to run initialize.py to create the stock database, and point to the correct location.")
 
 
     def __str__(self):
@@ -74,32 +92,60 @@ class PySimulator:
         for stock_record in data:
 
             # these variables are kept outside of the stock data (not passed directly on) but are passed on one at a time later
-            open_price = records[1]
-            close_price = records[2]
+            open_price = stock_record[1]
+            close_price = stock_record[2]
 
             stock_data = {
-                date : record[0],
-                price : open_price,
-                ticker : ticker,
-                transaction_fee : self.account.TRANSACTION_FEE
+                "date" : stock_record[0],
+                "price" : open_price,
+                "ticker" : ticker,
+                "transaction_fee" : self.account.TRANSACTION_FEE
             }
 
             # updating internal prices of trading account to reflect actual selling and buying values
-            self.latest_prices[stock_data.ticker] = open_price
+            self.latest_prices[stock_data["ticker"]] = open_price
             self.account.update(self.latest_prices)
 
             algorithm(stock_data, self.account, variables_copy)
 
-            self.latest_prices[stock_data.ticker] = close_price
+            self.latest_prices[stock_data["ticker"]] = close_price
             self.account.update(self.latest_prices)
             stock_data["price"] = close_price
 
             algorithm(stock_data, self.account, variables_copy)
 
 
-    def simulate(algorithm, time_start=None, time_end=None):
+    def simulate(self, algorithm, time_start=None, time_end=None):
         """
         Function that simulates the algorithm on data. Takes in an algorithm (function) that it will run, and optional times to start and end (timeframes within the data's range).
 
         The algorithm should take in an object (which contains the date, open price, close price, high, low, and volume), an "account" variable which will be the PyBank account passed in to the PySimulator object, and an "optional variables" object (which will be the initial object passed in to the PySimulator object)
         """
+
+        # connecting to database
+        self._connect_DB()
+
+        # normalizing the dates given (if any)
+        if time_start:
+            time_start = normalize_date(time_start)
+        else:
+            # setting it to zero
+            time_start = 0
+        if time_end:
+            time_end = normalize_date(time_end)
+        else:
+            # setting to really big number because I'm not sure how SQL handles the Python "infinity"
+            time_end = 1000000000000000000
+
+
+        # querying database for each ticker file
+        for ticker in self.ticker_list:
+
+            query = "SELECT * FROM \"" + str(ticker) + "\" WHERE date >= " + str(time_start) + " AND date <=" + str(time_end) + ";"
+
+            records = self.cursor.execute(query)
+            # backtesting individual tickers
+            print "Backtesting", ticker, "..."
+            self._iterate_algorithm(algorithm, records, ticker)
+
+        print "Backtesting done"
